09ab3dc6820c2008ddc36f87df760b1990f4879a
skychik <kir-esakov2011@mail.ru>
2017-12-09 21:55:03 +0300

    on my way

---
+++ src/main/java/beans/MainPageBean.java
@@ -0,0 +1,32 @@
+package beans;
+
+
+public class MainPageBean {
+	private double xValue;
+	private double yValue;
+	private double rValue;
+
+	public double getXValue() {
+		return xValue;
+	}
+
+	public void setXValue(double xValue) {
+		this.xValue = xValue;
+	}
+
+	public double getYValue() {
+		return yValue;
+	}
+
+	public void setYValue(double yValue) {
+		this.yValue = yValue;
+	}
+
+	public double getRValue() {
+		return rValue;
+	}
+
+	public void setRValue(double rValue) {
+		this.rValue = rValue;
+	}
+}
